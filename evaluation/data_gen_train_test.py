129eea46ed6f53ac7d445294de4925cdb799901d
samisnotinsane <sameen11@hotmail.com>
2021-08-29 00:31:36 +0100

    Fixed bug where train chb06  preictal was generating more than 3 files.

--- evaluation/data_gen_train_test.py
+++ evaluation/data_gen_train_test.py
@@ -20,6 +20,8 @@ df = pd.read_excel('train_test_data.xlsx')
 
 # core data generation loop
 with tqdm(total=len(df)) as pbar:
+    identifier = ['A', 'B', 'C'] # generate filename for Train -> chb06 -> preictal
+    counter = 0
     for index, row in df.iterrows():
         case = row['Case']
         filename = row['Filename']
@@ -50,8 +52,9 @@ with tqdm(total=len(df)) as pbar:
                     data = get_cropped_data(readpath, int(crop_start), int(crop_end))
                     writedir = saveroot + '/' + sset + '/' + case + '/' + cclass
                     Path(writedir).mkdir(parents=True, exist_ok=True)
-                    identifier = ''.join(random.choices(string.ascii_uppercase + string.digits, k=2))
-                    writepath = writedir + '/' + filename.split('.')[0] + '_' + identifier + '_' + cclass.lower() + savefmt
+                    # identifier = ''.join(random.choices(string.ascii_uppercase + string.digits, k=2))
+                    writepath = writedir + '/' + filename.split('.')[0] + '_' + identifier[counter] + '_' + cclass.lower() + savefmt
+                    counter += 1
                     np.save(writepath, data)
                 if cclass == 'Interictal':
                     readpath = root + '/' + case + '/' + filename
